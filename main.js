13e38b13d1e2eb517e345d534ca70cef92433526
Jae Early <Chrisspbacon@yahoo.com>
2020-09-06 21:33:15 -0500

    made a button for a single dice roll, another for a double dice roll, and a button for a counter to add up 1000 dice rolls and count how many times the numbers were rolled.  I also made a graph that appears when you click the counter button.

--- main.js
+++ main.js
@@ -1,9 +1,77 @@
-// Your Code Here!
+let random = document.getElementById("die");
+let status = document.getElementById("status");
+let die = Math.floor (Math.random() * 6);         
+      
 
-// Create a function that "rolls a die" and give you a number 1-6.
+function rollDie(){
+    
+    let dieroll = document.getElementById("die");
+    let die = Math.floor(Math.random() * 6) + 1;
+    dieroll.innerHTML =die;
+ 
+     }
 
-// Then roll two dice and add them together.
 
-// Do that 1000 times and create an array of how frequently each result occurrs.
+function rollDie2(){
 
-// Then render the output to the HTML page.
+    let die1 = document.getElementById("die1");
+    let die2 = document.getElementById("die2");
+    let status = document.getElementById("status");
+    let d1 = Math.floor (Math.random()*6) + 1;
+    let d2 = Math.floor (Math.random()*6) + 1;
+    let diceTotal = d1 + d2;
+    die1.innerHTML =d1;
+    die2.innerHTML =d2;
+    }
+     
+
+     let count=[0,0,0,0,0,0,0,0,0,0,0];
+     function rollOfDice(){
+      for(let num=1; num<= 1000; num++){
+      let d1 = Math.floor (Math.random()*6) + 1
+      let d2 = Math.floor (Math.random()*6) + 1
+      let value = d1 + d2 - 2;                 
+      count[value] = count [value] +1;
+
+      }
+
+     let roll = document.getElementById("rollOfDice");
+     let die = Math.floor(Math.random() * 6) + 1;
+     let numbers = [];
+
+     //roll.innerHTML = count;                
+     
+     for(let i = 0; i<= 11; i++){
+         numbers.push(count[i]);
+     }
+     for(let j = 0; j< count.length; j++){
+        let div = document.getElementById("bar " + (j))
+        div.append(" " + count[j])
+      console.log("bar " + j)
+
+     }
+
+     let mychart = document.getElementById("myChart").getContext("2d");
+
+     let myChart = new Chart(mychart, {
+        type: 'bar',
+        data: {
+          height: 200,
+          options: {
+            maintainAspectRatio: false,
+          },
+          width:  200,
+          labels: ['D2', 'D3',  'D4',  'D5',  'D6',  'D7',  'D8',  'D9',  'D10',  'D11',  'D12'],
+          datasets: [{
+            label: 'Dice Count',
+            data: numbers,
+            backgroundColor: "black",
+          }
+         ]
+        
+        },
+        
+    })
+  }
+    
+
